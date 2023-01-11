210c99f15e9f3fb18a3848e2488cb298c2df7f3f
sounchoi <ragi230@naver.com>
2023-01-11 16:19:33 +0900

    mini

--- minishell/mini/main.c
+++ minishell/mini/main.c
@@ -1,36 +1,30 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
+#include "minishell.h"
 
-int	minishell(int argc, char **envp);
-
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	if (argc != 1)
 	{
-		write(2, "you worng minishell start\n", 27);
+		write(2, "ERROR\nAn incorrect execution\n", 30);
 		return (1);
 	}
-	if (minishell(argc, envp) != 1)
-		return (1);
+	minishell(envp);
 	return (0);
 }
 
-int	minishell(int argc, char **envp)
+void	minishell(char **envp)
+{
+	t_tool tool;
+
+	tool.envp = mk_envp(envp);
+	tool.path = mk_path(envp);
+
+	start_mini(&tool);
+}
+
+void	start_mini(t_tool *tool)
 {
 	char	*str;
-	
-	while (1)
-	{
-		str = readline("minishell > ");
-		if (str != NULL)
-			printf("%s\n", str);
-		else
-			printf("exit");
-		add_history(str);
-		free(str);
-	}
-	return (0);
+
+	str = readline("prompt > ");
+	add_history(str);
 }
---
+++ minishell/mini/minishell.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sounchoi <sounchoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 23:37:59 by sounchoi          #+#    #+#             */
+/*   Updated: 2023/01/11 16:17:43 by sounchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <stdio.h>
+# include <stdio.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+typedef struct s_tool
+{
+	char	**path;
+	char	**envp;
+}	t_tool;
+
+typedef struct s_obj
+{
+	int		**fd;
+	char	**cmd;
+}	t_obj;
+
+typedef struct	s_token
+{
+	int	type_num;
+	int	value;
+	int	order;
+	struct s_token *next;
+}	t_token;
+
+#endif
