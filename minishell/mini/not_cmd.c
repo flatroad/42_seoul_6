929533cb067327bf135f82f335e876b436f87926
sounchoi <ragi230@naver.com>
2023-01-23 23:45:35 +0900

    mini

--- minishell/mini/not_cmd.c
+++ minishell/mini/not_cmd.c
@@ -3,26 +3,63 @@
 
 void	not_cmd(char *sv, t_token *token, char **str, t_tool tool)
 {
-	if (c == '<' || c == '>')
-		is_redi(c, token, str, tool);
+	if (**str == '<' || **str == '>')
+		is_redi(sv, token, str, tool);
 	else
-		is_unredi(c, token, str, tool);
+		not_redi(sv, token, str, tool);
 }
 
-void	is_redi(char c, t_token *token, char **str, t_tool tool)
+void	is_redi(char *sv, t_token *token, char **str, t_tool tool)
 {
-	if (c == '<')
-		is_rredi(c, token, str, tool);
+	if (**str == '<')
+	{
+		*str++;
+		is_leb(sv, token, str, tool);
+	}
 	else
-		is_lredi(c, token, str, tool);
+	{
+		*str++;
+		is_reb(sv, token, str, tool);
+	}
 }
 
-void	unredi(char c, t_token *token, char **str, t_tool tool)
+void	is_leb(char *sv, t_token *token, char **str, t_tool tool)
 {
-	if (c == '|')
-		is_pipe(c, token, str, tool);
+	if (*str + 1 == '<')
+	{
+		*str++;
+		add_token('<<');
+		is_heredoc(sv, token, str, tool);
+	}
 	else
-		is_unpipe(c, token, str, tool);
+	{
+		add_token('<');
+		is_infile(sv, token, str, tool);
+	}
+}
+
+void	is_heredoc(char *sv, t_token *token, char **str, t_tool tool)
+{
+	while (**str != ' ' || **str != 0)
+		*str++;
+	if (**str == 0)
+		add_token(NULL);
+	while (1)
+	{
+		if (!(**str == '|' || **str == '&' || **str == '<' || **str == '>' ||\ 
+		**str == ' ' || **str == 0))
+			add_token(sv);
+			return ;
+		
+	}
+}
+
+void	not_redi(char *sv, t_token *token, char **str, t_tool tool)
+{
+	if (**str == '|')
+		is_pipe(sv, token, str, tool);
+	else
+		not_pipe(sv, token, str, tool);
 }
 
 void	is_pipe(c, token, str, tool);
@@ -33,7 +70,7 @@ void	is_pipe(c, token, str, tool);
 	{
 		str++;
 		add_token('|');
-	}	
+	}
 }
 
 void	is_unpipe(char c, t_token *token, char **str, t_tool tool)
