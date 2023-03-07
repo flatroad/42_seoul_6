4f94cd99ef84669778debf9c2aafe90b18ef7330
sounchoi <ragi230@naver.com>
2023-03-07 15:00:46 +0900

    cpp

---
+++ CPP/cpp00/ex01/phonebook.hpp
@@ -0,0 +1,28 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include <iostream>
+#include <string>
+
+class phonebook
+{
+	private:
+		
+}
+
+class contact
+{
+	private:
+		std::string FirstName;
+		std::string LastName;
+		std::string NickName;
+		std::string PhoneNumber;
+		std::string DarkestSecret;
+	public:
+		InsertFN(std::string FirstName);
+		InsertLN(std::string LastName);
+		InsertNN()
+}
+
+
+#endif
