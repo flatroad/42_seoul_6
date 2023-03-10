270bb38216ab223189942e729a40368503e1c1eb
sounchoi <ragi230@naver.com>
2023-03-10 20:02:54 +0900

    cub3d

--- cub3d/main.c
+++ cub3d/main.c
@@ -56,6 +56,26 @@ int	map_validity(char **arr)
 	checker[0] = 0;
 	checker[1] = 0;
 	find_start_point(arr, checker);
-	if (outline_check(arr, checker) == 1)
+	if (start_map_check(arr, checker) == 1)
 		return (1);
 }
+
+void	find_start_point(char **arr, int checker[2])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (arr[i] != NULL)
+	{
+		j = 0;
+		if (arr[j] != 0)
+		{
+			if (arr[i][j] == 1)
+			j++;
+		}
+		i++;
+	}
+}
+
+void	start_map_check(char **arr, int)
---
+++ cub3d/map_vaildity.c
@@ -0,0 +1,102 @@
+#include "cub3d.h"
+
+typedef struct s_map
+{
+	int		xsize;
+	int		ysize;
+	char	**map;
+}	t_map;
+
+typedef struct	s_map_obj
+{
+	int	player;
+}	t_map_obj;
+
+int	map_vaildity(t_map *map_data, t_map_obj *map_obj);
+int	check_outline_point(t_map *map_data, int y, int x);
+int	check_four_point(t_map *map_data, int y, int x);
+void	map_obj_init(t_map_obj *map_obj);
+void	insert_map_obj(t_map_obj *map, char c);
+
+int	main()
+{
+	t_map		map_data;
+	t_map_obj	map_obj;
+	char		**str;
+
+	str = malloc(sizeof(char *) * 5);
+	str[0] = "1111";
+	str[1] = "1001";
+	str[2] = "1111";
+	str[3] = "    ";
+	str[4] = NULL;
+
+	map_data.map = str;
+	map_data.xsize = 5;
+	map_data.ysize = 5;
+	map_obj_init(&map_obj);
+	if(map_vaildity(&map_data, &map_obj) == 1)
+	{
+		printf("%s\n", "실패");
+		exit(0);
+	}
+	printf("%s\n", "성공");
+}
+
+void	map_obj_init(t_map_obj *map_obj)
+{
+	map_obj->player = 0;
+}
+
+int	map_vaildity(t_map *map_data, t_map_obj *map_obj)
+{
+	int		y;
+	int		x;
+
+	y = 0;
+	while (y < map_data->ysize - 1)
+	{
+		x = 0;
+		while (x < map_data->xsize)
+		{
+			insert_map_obj(map_obj, map_data->map[y][x]);
+			if (check_outline_point(map_data, y, x) == 1)
+				return (1);
+			if (check_four_point(map_data, y, x) == 1)
+				return (1);
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+void	insert_map_obj(t_map_obj *map, char c)
+{
+	if (c == 'N' || c == 'S' || c == 'E' || c == 'W')
+		map->player++;
+}
+
+int	check_outline_point(t_map *map_data, int y, int x)
+{
+	if (y != map_data->ysize - 1 && x != map_data->xsize - 1 && y != 0 && x != 0)
+		return (0);
+	if (map_data->map[y][x] == '0')
+		return (1);
+	return (0);
+}
+
+int	check_four_point(t_map *map_data, int y, int x)
+{
+	if (map_data->map[y][x] != ' ')
+		return (0);
+	if (y > 0 && map_data->map[y - 1][x] == '0')
+		return (1);
+	if (y < map_data->ysize - 2 && map_data->map[y + 1][x] == '0')
+		return (1);
+	if (x > 0 && map_data->map[y][x - 1] == '0')
+		return (1);
+	if (x < map_data->xsize - 1 && map_data->map[y][x + 1] == '0')
+		return (1);
+	return (0);
+}
