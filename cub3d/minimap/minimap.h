5e66a7ad2d7f5a5e4ab2f55e0ec1ce663cffa096
sounchoi <ragi230@naver.com>
2023-03-11 21:04:22 +0900

    cub3d

---
+++ cub3d/minimap/minimap.h
@@ -0,0 +1,11 @@
+#ifndef MINIMAP_H
+# define MINIMAP_H
+
+typedef struct s_map
+{
+	int		xsize;
+	int		ysize;
+	char	**map;
+}	t_map;
+
+#endif
--- mlx/main.c
+++ mlx/main.c
@@ -7,15 +7,25 @@ int	main()
 {
 	void	*mlx;
 	void	*ptr;
-	void	*img;
+	void	*img1;
+	void	*img2;
+	void	*img3;
+	void	*img4;
+	void	*img5;
 	int		img_width;
 	int		img_height;
 
 	mlx = mlx_init();
-	ptr = mlx_new_window(mlx, 1920, 1280, "test");
-	img = mlx_xpm_file_to_image(mlx, "mini.xpm", &img_width, &img_height);
-	printf("%p\n", img);
-	mlx_put_image_to_window(mlx, ptr, img, 0, 0);
+	ptr = mlx_new_window(mlx, 640, 480, "test");
+	img1 = mlx_xpm_file_to_image(mlx, "N_wall.xpm", &img_width, &img_height);
+	img2 = mlx_xpm_file_to_image(mlx, "S_wall.xpm", &img_width, &img_height);
+	img3 = mlx_xpm_file_to_image(mlx, "E_wall.xpm", &img_width, &img_height);
+	img4 = mlx_xpm_file_to_image(mlx, "W_wall.xpm", &img_width, &img_height);
+	img5 = mlx_new_image(mlx,600, 400);
+	mlx_put_image_to_window(mlx, ptr, img1, 0, 0);
+	mlx_put_image_to_window(mlx, ptr, img2, 64, 0);
+	mlx_put_image_to_window(mlx, ptr, img3, 128, 0);
+	mlx_put_image_to_window(mlx, ptr, img4, 192, 0);
 	mlx_loop(mlx);
 	return (0);
 }
