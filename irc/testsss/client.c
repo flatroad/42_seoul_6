8be491f4ddc5eb19147e3ab1f7c4efaa2155c7c7
sounchoi <ragi230@naver.com>
2023-05-27 17:50:18 +0900

    tets

--- irc/testsss/client.c
+++ irc/testsss/client.c
@@ -12,7 +12,8 @@ int main(int argc, char *argv[])
 	int sock;
 	struct sockaddr_in serv_addr;
 	char message[30];
-	int str_len;
+	int str_len = 0;
+	int idx = 0, read_len = 0;
 
 	if (argc != 3)
 	{
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	
-	sock = socket(PF_INET, SOCK_STREAM, 0);
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (sock == -1)
 		error_handling("socket() error");
 	
@@ -31,12 +32,18 @@ int main(int argc, char *argv[])
 
 	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("connect() error!");
-	
-	str_len = read(sock, message, sizeof(message) - 1);
-	if (str_len == -1)
-		error_handling("read() error!");
-	
+
+	read_len = read(sock, &message[++idx], 1);
+	while (read_len)
+	{
+		if (read_len == -1)
+			error_handling("read() error!");
+		
+		str_len += read_len;
+	} 
+
 	printf("Message from server : %s \n", message);
+	printf("Function read call count : %d \n", str_len);
 	close(sock);
 	return (0);
 }
